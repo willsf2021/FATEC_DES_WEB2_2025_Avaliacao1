e53c8d70928c88dd9138c1d521d5811578aaef2b
willsf2021 <willsf2021@gmail.com>
2025-03-31 19:19:57 -0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca Espaço do Saber</title>
+</head>
+
+<body>
+    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+
+
+
+
+    
+
+
+
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+
+</html>
